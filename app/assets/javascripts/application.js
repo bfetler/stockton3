07544c097d37d65b63202cbfaa6789b5dab1f046
bfetler <bfetler@gmail.com>
2012-09-07 21:28:00 -0700

    js clean up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,70 +22,44 @@ $(function() {
 });
 
 // notes from Iain at carbon 5
-// console.log() is very useful
-//   don't use console.log(obj + "text") if obj is object, not string
+// don't use console.log(obj + "text") if obj is object, not string
 function updateStockView() {
   $.getJSON("/getservice", function(data) {
-//  alert("aack!");
-//  $.each(data, function(obj) {
-//    alert(obj.value);
-//    alert("aaoooga!");
-//  });
-    console.log(data);
-//  $(data).each(function(obj) {
+//  console.log(data);
     $.each(data, function(i, obj) {
-      var td, tr;
+      var tr;
 
-//    td = $("#stocklist td.companysymbol:contains('" + obj.companysymbol + "')");
-      td = $("#stocklist td:contains('" + obj.companysymbol + "')");
-//    console.log(
-//      "#stocklist td:contains('" + obj.companysymbol + "')"
-//    );
-//    tr = td.parent();
-
+//    tr = $("#stocklist td:contains('" + obj.companysymbol + "')").parent();
       tr = $("#stocklist td.companysymbol:contains('" + obj.companysymbol + "')").parent();
       console.log(
         "#stocklist td.companysymbol:contains('" + obj.companysymbol + "').parent()"
       );
 
-      console.log("obj:");
+//    console.log("obj:");
       console.log(obj);
       console.log([obj.companysymbol, obj.value, obj.delta]);
-      console.log("tr:");
-      console.log(tr);
+//    console.log("tr:");
+//    console.log(tr);
 
-//    td.next().text(obj.value);  // it works, but brittle, depends on order
+//    td.next().text(obj.value);  // works, but depends on order
 //    td.next().next().text(obj.delta);
 
       tr.children(".value").text(obj.value); // works, independent of order
+//    tr.children(".delta").text(obj.delta);
       if (new Number(obj.delta) < 0) {
-        console.log("val is negative");
+        console.log("delta is negative");
         tr.children(".delta").text(obj.delta).css("color","red");
       } else {
-        console.log("val is positive");
+        console.log("delta is positive");
         tr.children(".delta").text(obj.delta).css("color","green");
       }
 
-      console.log("key val selector:");
-//    $(obj).each(function(key, val) {
-// could limit to specific keys, e.g. value delta
-      $.each(obj, function(key, val) {
-//      $("#stocklist td.companysymbol:eq('" + val + );
-        console.log([key, val]);
+//    console.log("key val selector:");
+//    $.each(obj, function(key, val) {
+//      console.log([key, val]);
 // see jQuery('element') link to jQuery function, jQuery(selector [,context])
-//      console.log( $("td." + key, tr) );
 //      $("td." + key, tr).text(val);
-//      if (key == "delta") {
-//        console.log("key is delta");
-//        if (new Number(val) < 0) {
-//          console.log("val is negative");
-//          $("td." + key, tr).css("color","red");
-//        } else {
-//          console.log("val is positive");
-//          $("td." + key, tr).css("color","green");
-//        }
-//      }
-      });
+//    });
     });
   });
   setTimeout(updateStockView, 10000);
