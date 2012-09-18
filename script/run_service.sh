b390f4d274122265997c9027d56793bdabfbe7f9
bfetler <bfetler@gmail.com>
2012-09-17 22:16:38 -0700

    get cron to work with rvm wrapper

--- script/run_service.sh
+++ script/run_service.sh
@@ -10,9 +10,29 @@
 
 # source $HOME/.rvm/scripts/rvm
 # rvm use 1.9.3-p125
+echo "start run_service" >> /tmp/stockton.tmp
 # /home/ub2/.rvm/scripts/rvm use 1.9.3-p125
+date >> /tmp/stockton.tmp
+which ruby >> /tmp/stockton.tmp
+cd /media/BLinux/ub2/rex/eg323/stockton/
+# echo "start bundle install" >> /tmp/stockton.tmp
+# bundle install >> /tmp/stockton.tmp  # probably not needed
 
-/media/BLinux/ub2/rex/eg323/stockton/script/rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1
+date >> /tmp/stockton.tmp
+echo "PATH: " $PATH >> /tmp/stockton.tmp
+echo "start rails runner" >> /tmp/stockton.tmp
+# /media/BLinux/ub2/rex/eg323/stockton/script/rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1
+# rvm wrapper ruby-1.9.3-p125@gemsetaa stockton rails
+#   => /home/ub2/.rvm/bin/stockton_rails   which is just a soft link
+#      to /home/ub2/.rvm/wrappers/ruby-1.9.3-p125/rails
+/home/ub2/.rvm/bin/stockton_rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1
+# /home/ub2/.rvm/wrappers/ruby-1.9.3-p125/rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1
+
+# gives error:
+# /media/BLinux/ub2/rex/eg323/stockton/config/boot.rb:1:in `require': no such file to load -- rubygems (LoadError)
+# 	from /media/BLinux/ub2/rex/eg323/stockton/config/boot.rb:1
+# 	from /media/BLinux/ub2/rex/eg323/stockton/script/rails:5:in `require'
+# 	from /media/BLinux/ub2/rex/eg323/stockton/script/rails:5
 
 # /home/ub2/.rvm/gems/ruby-1.9.3-p125/bin/rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1  # fails, Usage: rails new APP_PATH
 
