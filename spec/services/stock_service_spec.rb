43a0a7fc186e8286d2ab3890422873e33b145ac1
bfetler <bfetler@gmail.com>
2013-01-16 20:30:45 -0800

    clean up comments in stock_service_spec

--- spec/services/stock_service_spec.rb
+++ spec/services/stock_service_spec.rb
@@ -1,45 +1,33 @@
 require 'spec_helper'
-# require 'webmock/rspec'
-  require 'net/http'
-# WebMock.allow_net_connect!  # enables http in other files
+require 'net/http'
 
 describe StockService do
+  
   before(:each) do
     WebMock.allow_net_connect!
   end
+  
   after(:each) do
     WebMock.disable_net_connect!
   end
 
   it "should stub request_stocks" do
-# well this is true by definition, not helpful
     StockService.stub(:request_stocks).and_return("200")
     StockService.request_stocks('GOOG').should eq("200")
   end
 
   it "request_stocks should receive 'GOOG'" do
-# true by definition, not helpful
     StockService.should_receive(:request_stocks).with('GOOG')
     StockService.request_stocks('GOOG')
   end
 
   it "request_stocks should respond to fetch_uri stub" do
-# true by definition, not helpful
-#   res = Net::HTTPResponse.new()  # new(path, initheader=nil)
-#   rbody = "GOOG","Google Inc.","Aug 15 - <b>667.54</b>","-1.12 - -0.17%"
     rbody = '"GOOG","Google Inc.","Aug 15 - <b>667.54</b>","-1.12 - -0.17%"'
-#   http_mock = mock('Net::HTTPResponse')
     http_mock = double('Net::HTTPResponse')
     http_mock.stub(:code => '200', :message => "OK",
       :content_type => "text/html",
       :body => rbody)
-#   res.stub(:body).and_return(rbody)
     StockService.stub(:fetch_uri).and_return(http_mock)  # okay!
-#   Net::HTTP.stub(:get_response).and_return(http_mock)  # not quite right
-#     should return res, res.body = rbody
-#   stub_request(:get, ).with
-#   StockService.stub(:parse_response).and_return(res)
-#   StockService.parse_response(rbody).should eq(...)
     res = StockService.request_stocks('GOOG')
     res.code.should eq("200")
     res.message.should eq("OK")
@@ -47,17 +35,12 @@ describe StockService do
   end
 
   it "parse_response should respond to fetch_uri stub" do
-# true by definition, not helpful
-#   rbody = "GOOG","Google Inc.","Aug 15 - <b>667.54</b>","-1.12 - -0.17%"
     rbody = '"GOOG","Google Inc.","Aug 15 - <b>667.54</b>","-1.12 - -0.17%"'
-#   http_mock = mock('Net::HTTPResponse')
     http_mock = double('Net::HTTPResponse')
     http_mock.stub(:code => '200', :message => "OK",
       :content_type => "text/html",
       :body => rbody)
     StockService.stub(:fetch_uri).and_return(http_mock)  # okay!
-#   Net::HTTP.stub(:get_response).and_return(http_mock)  # not quite right
-#   StockService.parse_response(rbody).should eq(...)
     res = StockService.request_stocks('GOOG')
     res.code.should eq("200")
     res.body.should eq(rbody)
