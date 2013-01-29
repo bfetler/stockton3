31133942dccd1146050e9a2008c95a25d923571a
bfetler <bfetler@gmail.com>
2013-01-29 14:24:55 -0800

    clean up rake task, script

--- lib/tasks/stock_service.rake
+++ lib/tasks/stock_service.rake
@@ -2,24 +2,26 @@ namespace :stocks do
   
   task :default => [:update]
 
-  desc "update stocks"  
+  desc "update stocks"
   task :update => [:environment] do |t, args|
     puts "Stock count: #{Stock.count}"
     StockService.request
   end
   
-  desc "randomize stocks"  
+  desc "randomize stocks"
   task :random => [:environment] do |t, args|
-    Rake::Task['stocks:yayy'].invoke
+#   Rake::Task['stocks:yayy'].invoke
     puts "Stock count: #{Stock.count}"
     StockService.fake_request
   end
 
-  desc "yayy"  
-  task :yayy do
-    yays = ENV['YAYS'] || 2
-    puts "yayy!! " * yays
-  end
+#  desc "yayy"
+#  task :yayy do
+#    yays = ENV['YAYS'].to_i || 2
+#    puts "yayy!! " * yays
+#  end
+#
+#   $ YAYS=3; export YAYS
   
   file "execrun" do
     sh "../../script/run_service.sh"
--- script/run_service.sh
+++ script/run_service.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-echo "start run_service" >> /tmp/stockton.tmp
-date >> /tmp/stockton.tmp
-which ruby >> /tmp/stockton.tmp
-cd /media/BLinux/ub2/rex/eg323/stockton/
-date >> /tmp/stockton.tmp
-echo "PATH: " $PATH >> /tmp/stockton.tmp
+tmpfile=/tmp/stockton.tmp
 
-# to create stockton_rails:
+echo "START run_service" >> $tmpfile
+date >> $tmpfile
+which ruby >> $tmpfile
+cd /media/BLinux/ub2/rex/eg323/stockton/
+
+# to create stockton_rails wrapper:
 #   rvm wrapper ruby-1.9.3-p125@gemsetaa stockton rails
 #     => /home/ub2/.rvm/bin/stockton_rails   which is just a soft link
 #        to /home/ub2/.rvm/wrappers/ruby-1.9.3-p125/rails
 
-# echo "start rails runner" >> /tmp/stockton.tmp
-# /home/ub2/.rvm/bin/stockton_rails runner -e development "StockService.fake_request" >> /tmp/stockton.tmp 2>&1
-# /home/ub2/.rvm/bin/stockton_rails runner -e development "StockService.request" >> /tmp/stockton.tmp 2>&1
+# echo "start rails runner" >> $tmpfile
+# /home/ub2/.rvm/bin/stockton_rails runner -e development "StockService.fake_request" >> $tmpfile 2>&1
+# /home/ub2/.rvm/bin/stockton_rails runner -e development "StockService.request" >> $tmpfile 2>&1
 
-echo "start rake task" >> /tmp/stockton.tmp
-# rake stocks:random >> /tmp/stockton.tmp
-cd /media/BLinux/ub2/rex/eg323/stockton && /home/ub2/.rvm/gems/ruby-1.9.3-p125/bin/rake RAILS_ENV=development stocks:random >> /tmp/stockton.tmp
+rake_cmd=/home/ub2/.rvm/gems/ruby-1.9.3-p125/bin/rake 
+echo "$rake_cmd" >> $tmpfile
+echo "start rake task" >> $tmpfile
+$rake_cmd RAILS_ENV=development stocks:update >> $tmpfile 2>&1
+# $rake_cmd RAILS_ENV=development stocks:random >> $tmpfile 2>&1
 
+echo "DONE" >> $tmpfile
