0a56dcf47a781795ff0638c9be3b406a395fb7d7
bfetler <bfetler@gmail.com>
2013-02-04 23:03:47 -0800

    user integration tests

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,111 @@
+require 'spec_helper'
+
+describe "Integration: Users" do
+
+  describe "Create User" do
+    describe "failure" do
+      it "should not create a new user" do
+        lambda do
+#         visit 'root'
+          visit root_path
+          click_link "Create New User"
+          fill_in "Email",      :with => ""
+          fill_in "Password",   :with => ""
+          fill_in "Password confirmation",   :with => ""
+          click_button "Create New User"
+        end.should_not change(User, :count)
+      end
+
+      it "should have new user errors" do
+        visit root_path
+        click_link "Create New User"
+        fill_in "Email",      :with => ""
+        fill_in "Password",   :with => ""
+        fill_in "Password confirmation",   :with => ""
+        click_button "Create New User"
+#       page.should render_template('users')
+        page.should have_selector("div#error_explanation")
+        page.should have_selector("li", :content => "Email can't be blank")
+        page.should have_selector("li", :content => "Password can't be blank")
+      end
+    end
+
+    describe "success" do
+      it "should create a new user" do
+        lambda do
+#         visit 'users/sign_in'
+          visit root_path
+          click_link "Create New User"
+          fill_in "Email",      :with => "george@jetson.com"
+          fill_in "Password",   :with => "jaaaane"
+          fill_in "Password confirmation",   :with => "jaaaane"
+          click_button "Create New User"
+        end.should change(User, :count).by(1)
+      end
+
+      it "should show stock table" do
+#       visit 'users/sign_in'
+        visit root_path
+        click_link "Create New User"
+#       response.should render_template('users')
+        fill_in "Email",      :with => "george@jetson.com"
+        fill_in "Password",   :with => "jaaaane"
+        fill_in "Password confirmation",   :with => "jaaaane"
+        click_button "Create New User"
+#       page.should render_template('accelerators')
+        page.should have_selector("div.main_panel")
+        page.should have_selector("table.stock_panel")
+        page.should have_selector("div.action_panel")
+      end
+    end
+  end
+
+  describe "User Login" do
+
+    before { @user = FactoryGirl.create(:user) }
+
+    describe "failure" do
+      it "should not log in an invalid user" do
+        lambda do
+          visit root_path
+          fill_in "Email",      :with => ""
+          fill_in "Password",   :with => ""
+          click_button "Sign In"
+        end.should_not change(User, :count)
+      end
+
+      it "should show error message for invalid user" do
+        visit root_path
+        fill_in "Email",      :with => ""
+        fill_in "Password",   :with => ""
+        click_button "Sign In"
+#       response.should render_template('users/sessions/new')
+        page.should have_selector("p", :content => "Invalid email or password.")
+      end
+    end
+
+    describe "success" do
+      it "should log in a factory user" do
+        lambda do
+          visit root_path
+          fill_in "Email",      :with => @user.email
+          fill_in "Password",   :with => @user.password
+          click_button "Sign In"
+        end.should_not change(User, :count)
+      end
+
+      it "should show stock table for a factory user" do
+        visit root_path
+        fill_in "Email",      :with => @user.email
+        fill_in "Password",   :with => @user.password
+        click_button "Sign In"
+#       response.should render_template('accelerators')
+        page.should have_selector("div.main_panel")
+        page.should have_selector("table.stock_panel")
+        page.should have_selector("div.action_panel")
+      end
+    end
+
+  end
+
+end
